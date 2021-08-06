7ec01109312213d1c05ff68f4ba116955d0e5f40
Frans <frans_421_12@hotmail.com>
2021-08-05 22:44:14 -0500

    feat: get, list and delete questionnaires

--- QuizAPI/Persistence/Repositories/CuestionarioRepository.cs
+++ QuizAPI/Persistence/Repositories/CuestionarioRepository.cs
@@ -1,4 +1,5 @@
-﻿using QuizAPI.Domain.IRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using QuizAPI.Domain.IRepositories;
 using QuizAPI.Domain.Models;
 using QuizAPI.Persistence.Context;
 using System;
@@ -20,6 +21,52 @@ namespace QuizAPI.Persistence.Repositories
         {
             _context.Add(cuestionario);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Cuestionario>> GetListCuestionarioByUser(int idUsuario)
+        {
+            var listCuestionario = await _context.Cuestionario.Where(x => x.Activo == 1
+                                                    && x.UsuarioId == idUsuario).ToListAsync();
+            return listCuestionario;
+        }
+
+        public async Task<Cuestionario> GetCuestionario(int idCuestionario)
+        {
+            var cuestionario = await _context.Cuestionario.Where(x => x.Id == idCuestionario
+                                                                && x.Activo == 1)
+                                                                .Include(x => x.listPreguntas)
+                                                                .ThenInclude(x => x.listRespuestas)
+                                                                .FirstOrDefaultAsync();
+            return cuestionario;
+        }
+
+        public async Task<Cuestionario> BuscarCuestionario(int idCuestionario, int idUsuario)
+        {
+            var cuestionario = await _context.Cuestionario.Where(x => x.Id == idCuestionario && x.Activo == 1 && x.UsuarioId == idUsuario).FirstOrDefaultAsync();
+            return cuestionario;
+        }
+
+        public async Task EliminarCuestionario(Cuestionario cuestionario)
+        {
+            cuestionario.Activo = 0;
+            _context.Entry(cuestionario).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Cuestionario>> GetListCuestionarios()
+        {
+            var listCuestionarios = await _context.Cuestionario.Where(x => x.Activo == 1)
+                                                                .Select(o => new Cuestionario
+                                                                {
+                                                                    Id = o.Id,
+                                                                    Nombre = o.Nombre,
+                                                                    Descripcion = o.Descripcion,
+                                                                    FechaCreacion = o.FechaCreacion,
+                                                                    Usuario = new Usuario { NombreUsuario = o.Usuario.NombreUsuario }
+                                                                })
+
+                                                                .ToListAsync();
+            return listCuestionarios;
         }
     }
 }
